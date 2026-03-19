6516446bac73c590c3bc5c1ccb7a53b55945a7fe
AbdulrhmanBeatz <abooooody40@gmail.com>
2026-03-19 16:20:44 +0300

    refactor: improve formatting and structure of fetchCPUStats and fetchMemStats functions

--- src/core/ResourceInfo.cpp
+++ src/core/ResourceInfo.cpp
@@ -23,11 +23,14 @@ void ResourceInfo::fetchInfo() {
 
 void ResourceInfo::fetchCPUStats() {
     std::chrono::steady_clock::time_point currentTime = std::chrono::steady_clock::now();
+
     std::string path = std::format(PROC_CPU_STATS_PATH, this->pID);
     std::string output = open_file(path);
     std::vector<std::string> stats = split_string(output, " ");
+
     long utime = std::stoi(stats.at(13));
     long stime = std::stoi(stats.at(14));
+
     if(this->cpuUsage.previousUtime == 0 && this->cpuUsage.previousStime == 0) {
         // First sample, no calculation can be done.
         this->cpuUsage.previousUtime = utime;
@@ -36,13 +39,30 @@ void ResourceInfo::fetchCPUStats() {
     } else {
         long deltaCPUTicks = (utime + stime) - (this->cpuUsage.previousUtime + this->cpuUsage.previousStime);
         double elapsedSeconds = std::chrono::duration<double>(currentTime - this->cpuUsage.previousTime).count();
+
         long numberOfCPUs = sysconf(_SC_NPROCESSORS_ONLN);
+
         double totalTicks = elapsedSeconds * HZ * numberOfCPUs;
+
         this->cpuUsage.cpuUtilizationRate = (totalTicks > 0.0)
         ? (deltaCPUTicks / totalTicks) * 100.0
         : 0.0;
+
         this->cpuUsage.previousUtime = utime;
         this->cpuUsage.previousStime = stime;
         this->cpuUsage.previousTime = currentTime;
     }
+}
+
+void ResourceInfo::fetchMemStats() {
+    std::string path = std::format(PROC_MEM_STATS_PATH, this->pID);
+    std::string output = open_file(path);
+    std::vector<std::string> statm = split_string(output, " ");
+
+    this->memSize = statm.at(0);
+
+}
+
+void ResourceInfo::fetchIOStats() {
+    
 }
