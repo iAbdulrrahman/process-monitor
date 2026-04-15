385c7a873c3d8f3be611aadc3acac4d3aba87804
Raiyan Reza <lraiyanrezal@gmail.com>
2026-04-15 15:28:38 +0300

    feat-login-page-with summary page

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "core/ProcessMonitor.h"
-#include "ui/mainwindow.h"
+#include "ui/MainWindow.h"
+
+#include "LoginWindow.h"  //for login window   first
 
 #include <QApplication>
 #include <QTableWidget>
@@ -9,8 +11,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv); //anything between these two lines is so run the application
-    MainWindow w;
-    w.show();
+    //MainWindow w; // this will open summaryWindow first
+    //w.show();
+    LoginDialog loginWindow;
+    loginWindow.show();
     return a.exec(); //anything between these two lines is so run the application
 }
 
