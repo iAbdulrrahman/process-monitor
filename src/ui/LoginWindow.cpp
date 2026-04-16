8b026deeb76adda1753adfcaedd0096c6d5dd5c1
Raiyan Reza <lraiyanrezal@gmail.com>
2026-04-16 09:54:51 +0300

    feat-asterisk-password

--- src/ui/LoginWindow.cpp
+++ src/ui/LoginWindow.cpp
@@ -8,6 +8,9 @@ LoginDialog::LoginDialog(QWidget *parent)
     , ui(new Ui::LoginDialog)
 {
     ui->setupUi(this);
+
+    // Programiticly  HIDE password
+    ui->lineEdit_password->setEchoMode(QLineEdit::Password); //*
 }
 
 LoginDialog::~LoginDialog()
@@ -15,18 +18,40 @@ LoginDialog::~LoginDialog()
     delete ui;
 }
 
+// void LoginDialog::on_pushButton_login_clicked()
+// {
+//     QString username = ui->lineEdit_username->text();
+//     QString password = ui->lineEdit_password->text();
+
+//     if(username == "admin" && password == "admin"){
+//         QMessageBox::information(this, "login", "login successfull for admin");
+//         hide();
+//         // This requires 'mainwindow' to be defined in LoginWindow.h
+//         mainwindow = new MainWindow(this);
+//         mainwindow->show();
+//     } else {
+//         QMessageBox::warning(this, "login", "Unsuccessful login");
+//     }
+// }
+
 void LoginDialog::on_pushButton_login_clicked()
 {
     QString username = ui->lineEdit_username->text();
     QString password = ui->lineEdit_password->text();
 
     if(username == "admin" && password == "admin"){
-        QMessageBox::information(this, "login", "login successfull for admin");
-        hide();
-        // This requires 'mainwindow' to be defined in LoginWindow.h
-        mainwindow = new MainWindow(this);
+        // 1. Hide the login window IMMEDIATELY
+        this->hide();
+
+        // 2. Now show the success message
+        QMessageBox::information(this, "login", "Login successful for admin");
+
+        // 3. Create and show MainWindow (with memory leak protection)
+        mainwindow = new MainWindow(); // Removed 'this' to allow independent closing
+        mainwindow->setAttribute(Qt::WA_DeleteOnClose);
         mainwindow->show();
     } else {
         QMessageBox::warning(this, "login", "Unsuccessful login");
     }
 }
+
